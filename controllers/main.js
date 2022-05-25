53f1dcb1775b33a6d2b9f80a5e7d91db688bcfff
maualice <alicemauroe@gmail.com>
2022-05-25 18:17:53 -0300

    probando metodo sign de jwt

--- controllers/main.js
+++ controllers/main.js
@@ -1,15 +1,33 @@
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
-  res.send('Fake Login/Registered/Signup Route');
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    //usando bd esto se podria validar con mongoose.Otra forma es usando joi
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
+
+  //just for demo, normally provided by DB!!!!
+  const id = new Date().getDate();
+
+  // try to keep payload small, better experience for user
+  // just for demo, in production use long, complex and unguessable string value!!!!!!!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  res.status(200).send({ msg: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
+  console.log(req.headers);
   const luckyNumber = Math.floor(Math.random() * 100);
-  res
-    .status(200)
-    .json({
-      msg: `Hello,John Doe`,
-      secret: `Here is your authorized data,your lucky number is ${luckyNumber}`,
-    });
+  res.status(200).json({
+    msg: `Hello,John Doe`,
+    secret: `Here is your authorized data,your lucky number is ${luckyNumber}`, // para demostrar que son diferentes requests(al dar numeros diferentes)
+  });
 };
 
 module.exports = {
