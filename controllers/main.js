5429e33eab1cf1daad8654f911331fd555cde835
maualice <alicemauroe@gmail.com>
2022-05-26 02:37:37 -0300

    configurando ruta dashboard para obtener token y decodificarlo, ademas de manejar posibles errores

--- controllers/main.js
+++ controllers/main.js
@@ -22,7 +22,22 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new CustomAPIError('No token provided', 401);
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+  } catch (error) {
+    throw new CustomAPIError('Not authorized to access this route', 401); // si por ejemplo el token expiro
+  }
+
+  //console.log(req.headers);
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello,John Doe`,
